48ba62c47655bb9141b4dd5c90f29666a9640e0b
Bryan Hain <bryanhain97@gmail.com>
2022-09-29 22:55:00 +0200

    add mixins and build thoughtsform

--- components/ThoughtsForm.tsx
+++ components/ThoughtsForm.tsx
@@ -1,27 +1,34 @@
 import styles from '../styles/ThoughtsForm.module.sass';
-import { Importance } from '../types/Note';
-import { SyntheticEvent, useState } from 'react'
+import Note, { Importance } from '../types/Note';
+import { ChangeEvent, MouseEventHandler, useEffect, useState } from 'react'
 import { FaOctopusDeploy } from 'react-icons/fa'
 
 const ThoughtsForm = () => {
-    const [importance, setImportance] = useState<Importance>(Importance.Low)
-
-    const getImportance = (e: SyntheticEvent) => {
-        console.log(e)
+    const [note, setNote] = useState<Note>({
+        title: '',
+        text: '',
+        importance: Importance.Not
+    })
+    const updateNote = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setNote((prevNote) => ({ ...prevNote, [e.target.name]: e.target.value }))
     }
+
+    useEffect(() => {
+        console.log(note)
+    }, [note])
     return (
         <form className={styles.thoughtsForm}>
             <h5>create thought</h5>
             <div className={styles.thoughtsForm_field}>
                 <label htmlFor="title">title</label>
-                <input type="text" id="title"></input>
+                <input type="text" id="title" name="title" onChange={updateNote}></input>
             </div>
             <div className={styles.thoughtsForm_field}>
-                <label htmlFor="text">text</label>
-                <input type="text" id="text"></input>
+                <label htmlFor="textarea">text</label>
+                <textarea className="textarea" id="textarea" name="text" onChange={updateNote}></textarea>
             </div>
             <div className={styles.thoughtsForm_importanceLevel}>
-                <FaOctopusDeploy className={styles.thoughtsForm_importanceLevel_Not} onClick={getImportance} />
+                <FaOctopusDeploy className={styles.thoughtsForm_importanceLevel_Not} />
                 <FaOctopusDeploy className={styles.thoughtsForm_importanceLevel_Low} />
                 <FaOctopusDeploy className={styles.thoughtsForm_importanceLevel_Medium} />
                 <FaOctopusDeploy className={styles.thoughtsForm_importanceLevel_High} />
